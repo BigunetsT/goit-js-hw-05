444edb856ec68f0e0484d394f29c974dd67ebc99
Tetyana Bigunets <tanyuschamail83@gmail.com>
2020-12-25 19:08:59 +0200

    Домашняя работа №5

---
+++ JS/task-3.js
@@ -0,0 +1,45 @@
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+//При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+
+class Storage {
+  constructor([...args]) {
+    this.items = Object.values(args);
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      const itemIndex = this.items.indexOf(item);
+      this.items.splice(itemIndex, 1);
+    } else {
+      console.log('такого элемента нет');
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.log(items);
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
